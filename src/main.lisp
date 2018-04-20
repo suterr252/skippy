8eabc8b454cdcb3dbc06c7cfe15f59cd868dfff0
Rex Suter <dale@Rexs-MacBook-Air.local>
2018-04-19 22:55:32 -0400

    leading 0s (again)

--- src/main.lisp
+++ src/main.lisp
@@ -13,12 +13,15 @@
           collect (subseq data (* jump beg)
                           (if end (* jump end) nil)))))
 
-(defun reformat-decoded-polyline (list)
-  (loop for latlng in list
-        collect (format nil "~a,~a" (car latlng) (cadr latlng))))
+(defun flatten-once (list)
+  (let ((flattened ()))
+    (loop for sublist in list do
+      (setf flattened (append flattened sublist)))
+    flattened))
 
-(defun combine-idx-and-loc (idx other)
-  (format nil "~a,~a" idx other))
+
+(defun build-filename (idx other)
+  (format nil "~4,'0d,~a" idx other))
 
 (defun threaded-get-requests (list)
   (loop for chunk in list
@@ -47,9 +50,12 @@
 (defun decode-polylines (polylines)
   (loop for polyline in polylines collect (decode polyline)))
 
-(defun reformat-decoded-polylines (polylines)
+(defun add-individual-heading (decoded-polyline)
+  (append decoded-polyline "188"))
+
+(defun add-headings (polylines)
   (loop for polyline in polylines
-        collect (reformat-decoded-polyline polyline)))
+        collect (add-individual-heading polyline)))
 
 (defun main (origin destination)
   (format t "beginning route from ~a to ~a~%" origin destination)
@@ -57,23 +63,27 @@
          (to (replace-spaces-with-pluses destination))
          (directions-api-response (get-directions-api-response from to))
          (encoded-polylines (parse-response directions-api-response))
-         (decoded-polylines (decode-polylines encoded-polylines))
-         (formatted-latlng-lists (reformat-decoded-polylines decoded-polylines)))
-
+         (decoded-polylines-lists (decode-polylines encoded-polylines))
+         (decoded-polylines (flatten-once decoded-polylines-lists)))
     (cl-fad:delete-directory-and-files tmp-dir :if-does-not-exist :ignore)
+    (print "##### decoded polylines ####")
+    (print decoded-polylines);; nesty
 
-    ;; TODO: Dynamic headings (camera angle)
     (defparameter urls-and-paths ())
     (let ((file-count 0))
-      (loop for latlng-list in formatted-latlng-lists do
-        (loop for latlong in latlng-list do
-          (let ((target-url (image-url latlong "165"))
-                (save-path (get-save-path
-                            (combine-idx-and-loc
-                             file-count latlong))))
-            (setf urls-and-paths
-                  (append urls-and-paths (list (list target-url save-path))))
-            (setf file-count (+ 1 file-count))))))
+      (loop for latlng-list in decoded-polylines do
+        (let* ((lat (car latlng-list))
+               (lng (cadr latlng-list))
+               (latlong (format nil "~a,~a" lat lng))
+               (heading "165")
+               (target-url (image-url latlong heading))
+               (save-path (get-save-path (build-filename file-count latlong))))
+          (setf urls-and-paths
+                (append urls-and-paths (list (list target-url save-path)))))
+        (incf file-count)))
+
+    (print "###### urls-and-paths #######")
+    (print urls-and-paths)
 
     (let* ((partitioned-list (partition-in 4 urls-and-paths))
            (threads (threaded-get-requests partitioned-list)))
@@ -88,12 +98,6 @@
   "Lawson Computer Science Building, 305 N University St, West Lafayette, IN 47907"
   "John W. Hicks Undergraduate Library, 504 W State St, West Lafayette, IN 47907"))
 
-;; (Single Threaded )Evaluation took: 13.235 seconds of real time
-;; (Pooled threads [5])Evaluation took: 12.348 seconds of real time
-;; (Pooled threads [10])Evaluation took: 9.877 seconds of real time
-;; (Pooled threads [50]) Evaluation took: 23.023 seconds of real tim
-;; (basic use of 2 threads) Evaluation took:  4.014 seconds of real time
-;; (basic use of 4 threads) Evaluation took:  4.595 seconds of real time
 
 (time (main
        "2813 Bush Street San Francisco, CA 94115"
@@ -103,12 +107,6 @@
   "2400 Yeager Rd, West Lafayette, IN 47906"
   "900 John R Wooden Dr, West Lafayette, IN 47907"))
 
-;; (Single threaded) Evaluation took: 75.316 seconds of real time
-;; (Pooled threads [5]) Evaluation took: 61.992 seconds of real time
-;; (Pooled threads [10]) Evaluation took: 65.041 seconds of real time
-;; (Pooled threads [50]) Evaluation took: 127.582 seconds of real time
-;; (basic use of 2 threads) Evaluation took: 37.509 seconds of real time
-;; (basic use of 4 threads) Evaluation took: 20.917 seconds of real time
 
 
 
